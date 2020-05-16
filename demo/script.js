ed1a3ca78d7ad565f91667e95cc99e67c9570802
Susanna <syuzi22@gmail.com>
2020-05-16 21:06:12 +0300

    added block try catch for debugging in ios

--- demo/script.js
+++ demo/script.js
@@ -12,7 +12,17 @@ let options = {
     // formatMoney: (number) => new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(number)
 };
 
+try {
+
 let myCalc = new CreditCalc(node, options);
 myCalc.makeNodes();
 
+}catch(error) {
+    alert(error);
+}
+
+
+
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+
+

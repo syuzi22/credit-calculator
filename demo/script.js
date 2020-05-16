8e3d761a39c8e8d8d4387cff170afc95edf09230
Susanna <syuzi22@gmail.com>
2020-05-16 23:43:21 +0300

    Fixed early access to DOM

--- demo/script.js
+++ demo/script.js
@@ -7,23 +7,22 @@ window.onerror = function (msg, url, lineNo, columnNo, error) {
 };
 
 try {
-    let node = document.querySelector(".credit-calc");
-
-    let options = {
-        rate: "15%",
-        years: ["1 год", "2 года", "3 года", "4 года", "5 лет"],
-        rangeMin: 1e5,
-        rangeMax: 1e6,
-        rangeStep: 1e4,
-        // formatMoney: (number) => number + ' руб'
-        // formatMoney: (number) => new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(number)
-    };
-
-    let myCalc = new CreditCalc(node, options);
-    myCalc.makeNodes();
-
-    // document.addEventListener("DOMContentLoaded", runCalc);
     // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+    document.addEventListener("DOMContentLoaded", () => {
+        let node = document.querySelector(".credit-calc");
+
+        let options = {
+            rate: "15%",
+            years: ["1 год", "2 года", "3 года", "4 года", "5 лет"],
+            rangeMin: 1e5,
+            rangeMax: 1e6,
+            rangeStep: 1e4,
+            // formatMoney: (number) => number + ' руб'
+            // formatMoney: (number) => new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(number)
+        };
+        let myCalc = new CreditCalc(node, options);
+        myCalc.makeNodes();
+    });
 } catch (e) {
     alert("Caught error: " + e);
 }

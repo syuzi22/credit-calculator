625c6d3343226a358f7ef42a61109d70c74dac42
Susanna <syuzi22@gmail.com>
2020-05-16 18:11:22 +0300

    Initial commit

---
+++ demo/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let node = document.querySelector(".credit-calc");
+
+let options = {
+    rate: "15%",
+    years: ["1 год", "2 года", "3 года", "4 года", "5 лет"],
+    rangeMin: 1e5,
+    rangeMax: 1e6,
+    rangeStep: 1e4,
+    // formatMoney: (number) => number + ' руб'
+    // formatMoney: (number) => new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(number)
+};
+
+let myCalc = new CreditCalc(node, options);
+myCalc.makeNodes();
+
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat

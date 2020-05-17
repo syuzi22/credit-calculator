110da5b0dee3031a931fa3eb08ba1bcf09132fc6
Susanna <syuzi22@gmail.com>
2020-05-17 16:31:01 +0300

    debug

--- demo/script.js
+++ demo/script.js
@@ -5,8 +5,8 @@ let node = document.querySelector(".credit-calc");
 let options = {
     rate: "15%",
     years: ["1 year", "2 years", "3 years", "4 years", "5 years"],
-    rangeMin: 1e5,
-    rangeMax: 1e6,
+    rangeMin: 1e4,
+    rangeMax: 1e5,
     rangeStep: 1e4,
     // formatMoney: (number) => number + ' руб'
     // formatMoney: (number) => new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(number)
